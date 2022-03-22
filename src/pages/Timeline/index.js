63c5208b404807541d0607044119ff0303f1e9d5
renaatosalgado <renaato.salgado@gmail.com>
2022-03-22 11:02:10 -0300

    fat: publish post

--- src/pages/Timeline/index.js
+++ src/pages/Timeline/index.js
@@ -14,31 +14,31 @@ import {
   Publish,
 } from "./style";
 
+import api from "../../services/api";
+
 export default function Timeline() {
   const [loading, setLoading] = useState(false);
   const [url, setUrl] = useState("");
   const [description, setDescription] = useState("");
 
   function publishPost(event) {
-    // event.preventDefault();
-    // setLoading(true);
-    // const body = {
-    //   description,
-    //   url,
-    // };
-    // postCreatePost(body, config)
-    //   .then((res) => {
-    //     setLoading(false);
-    //     setText("");
-    //     setLink("");
-    //     window.location.reload();
-    //   })
-    //   .catch(() => {
-    //     alert(
-    //       "There was an error while posting your link. Repeat the procedure."
-    //     );
-    //     setLoading(false);
-    //   });
+    event.preventDefault();
+    setLoading(true);
+
+    api
+      .createPost({ description, url })
+      .then((res) => {
+        setLoading(false);
+        setDescription("");
+        setUrl("");
+        window.location.reload();
+      })
+      .catch(() => {
+        alert(
+          "There was an error while posting your link. Repeat the procedure."
+        );
+        setLoading(false);
+      });
   }
 
   return (
@@ -48,7 +48,7 @@ export default function Timeline() {
           <Title>timeline</Title>
           <CreatePost>
             <CreatePostImg>
-              {/* <ProfilePic src={user.user.avatar} alt="" /> */}
+              {/* <ProfilePic src={} alt="" /> */}
             </CreatePostImg>
             <Form onSubmit={publishPost}>
               <p>What do you have to share today?</p>
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
+import dotenv from "dotenv";
+dotenv.config();
 
-const BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const BASE_URL = "http://localhost:5000"
 
 function createConfig(token) {
     return {
