93491496aaad2e1b4205db94fba2281b863b5f6b
Crystopher Carpenter <crystopher.stanley.carpenter@gmail.com>
2022-03-23 06:58:19 -0300

    refactor: changing variables name

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,7 +7,7 @@ import { IoChevronUpOutline, IoChevronDownOutline } from 'react-icons/io5';
 import { IconContext } from 'react-icons';
 
 function Header() {
-    const [logoutButtonOpen, setLogoutButtonOpen] = useState(false);
+    const [isMenuOpen, setisMenuOpen] = useState(false);
     const ref = useRef(null);
     const navigation = useNavigate();
     const { pathname } = useLocation();
@@ -18,29 +18,25 @@ function Header() {
     const isHomePage = pathname === `/` || pathname === `/sign-up`;
 
     useEffect(() => {
-        const checkIfClickedOutside = (e) => {
-            if (
-                logoutButtonOpen &&
-                ref.current &&
-                !ref.current.contains(e.target)
-            ) {
-                setLogoutButtonOpen(false);
+        const checkClick = (e) => {
+            if (isMenuOpen && ref.current && !ref.current.contains(e.target)) {
+                setisMenuOpen(false);
             }
         };
 
-        document.addEventListener('click', checkIfClickedOutside);
+        document.addEventListener('click', checkClick);
 
         return () => {
-            document.removeEventListener('click', checkIfClickedOutside);
+            document.removeEventListener('click', checkClick);
         };
-    }, [logoutButtonOpen]);
+    }, [isMenuOpen]);
 
     async function logoutHandler() {
         try {
             await api.logout(user.id);
             login('');
             navigation('/');
-            setLogoutButtonOpen(false);
+            setisMenuOpen(false);
         } catch (error) {
             console.log(error);
             alert('Tente novamente');
@@ -51,14 +47,14 @@ function Header() {
         !isHomePage && (
             <TopBar>
                 <Logo>linkr</Logo>
-                <Arrow onClick={() => setLogoutButtonOpen(!logoutButtonOpen)}>
+                <Arrow onClick={() => setisMenuOpen(!isMenuOpen)}>
                     <IconContext.Provider
                         value={{
                             color: 'white',
                             size: '2em',
                         }}
                     >
-                        {logoutButtonOpen ? (
+                        {isMenuOpen ? (
                             <IoChevronUpOutline />
                         ) : (
                             <IoChevronDownOutline />
@@ -68,9 +64,9 @@ function Header() {
                 <Photo
                     src={`${user ? user.image : ''}`}
                     alt="Foto"
-                    onClick={() => setLogoutButtonOpen(!logoutButtonOpen)}
+                    onClick={() => setisMenuOpen(!isMenuOpen)}
                 />
-                {logoutButtonOpen && (
+                {isMenuOpen && (
                     <LogoutButton onClick={() => logoutHandler()} ref={ref}>
                         Lougout
                     </LogoutButton>
