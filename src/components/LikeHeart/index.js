28c990a3e150e0828739de30425edd8672b4d193
Crystopher Carpenter <crystopher.stanley.carpenter@gmail.com>
2022-03-26 18:14:46 -0300

    fix: post login errors in Post component

--- src/components/LikeHeart/index.js
+++ src/components/LikeHeart/index.js
@@ -1,80 +1,92 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 import ReactTooltip from 'react-tooltip';
-import { HeartIcon, HeartIconSelect, LikeHeartWrapper, LikeText } from "./style";
-import useAuth from "../../hooks/useAuth";
-import api from "../../services/api";
+import {
+    HeartIcon,
+    HeartIconSelect,
+    LikeHeartWrapper,
+    LikeText,
+} from './style';
+import useAuth from '../../hooks/useAuth';
+import api from '../../services/api';
 
 export default function LikeHeart({ like, setLike, postId }) {
-  const { auth } = useAuth();
-  const [refresh, setRefresh] = useState(false);
-  const [totalLikes, setTotalLikes] = useState(0);
-  const [nameTooltip, setNameTooltip] = useState("");
-  const [disabled, setDisabled] = useState(false);
+    const { auth } = useAuth();
+    const [refresh, setRefresh] = useState(false);
+    const [totalLikes, setTotalLikes] = useState(0);
+    const [nameTooltip, setNameTooltip] = useState('');
+    const [disabled, setDisabled] = useState(false);
 
-  useEffect(() => {
-    api.totalLikes(postId, auth?.token)
-      .then(({ data }) => {
-        setTotalLikes(data);
-        if (data === 0) setDisabled(true);
-        else setDisabled(false);
-      })
+    useEffect(() => {
+        api.totalLikes(postId, auth?.token).then(({ data }) => {
+            setTotalLikes(data);
+            if (data === 0) setDisabled(true);
+            else setDisabled(false);
+        });
 
-    api.getTwoNames(postId, auth?.token)
-      .then(({ data }) => {
-        mountNamesForTheTooltip(data);
-      })
+        api.getTwoNames(postId, auth?.token).then(({ data }) => {
+            mountNamesForTheTooltip(data);
+        });
 
-    api.checkLikeUser(postId, auth?.token)
-      .then(({ data }) => {
-        setLike(data);
-      })
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [postId, auth?.token, refresh, setLike]);
+        api.checkLikeUser(postId, auth?.token).then(({ data }) => {
+            setLike(data);
+        });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [postId, auth?.token, refresh, setLike]);
 
-  async function handleLike() {
-    await api.toggleLike({ like, postId }, auth?.token);
-    setLike(!like);
-    setRefresh(!refresh);
-  }
-
-  function mountNamesForTheTooltip(data) {
-    const { userLiked, likes } = data[data.length - 1];
-    data.pop();
-
-    let textTooltip = '';
-
-    const LOGGED_USER_LIKED = userLiked === true;
-    const TOTAL = likes.total - 2;
-    const CHECK_PLURAL = TOTAL > 1 ? '' : 's';
-
-    if (data.length > 1) {
-      LOGGED_USER_LIKED
-        ? textTooltip += `Você, ${data[0].name}`
-        : textTooltip += `${data[0].name}, ${data[1].name}`;
-
-      TOTAL > 0
-        ? textTooltip += ` e outra${CHECK_PLURAL} ${TOTAL} pessoa${CHECK_PLURAL}`
-        : textTooltip += ` curtiu`;
-
-    } else if (data.length === 1) {
-      LOGGED_USER_LIKED
-        ? textTooltip += `Você e ${data[0].name}`
-        : textTooltip += `${data[0].name}`;
-
-      textTooltip += ` curtiu`;
-
-    } else {
-      LOGGED_USER_LIKED && (textTooltip += `Você curtiu`);
+    async function handleLike() {
+        await api.toggleLike({ like, postId }, auth?.token);
+        setLike(!like);
+        setRefresh(!refresh);
     }
 
-    setNameTooltip(textTooltip);
-  }
+    function mountNamesForTheTooltip(data) {
+        const { userLiked, likes } = data[data.length - 1];
+        data.pop();
 
-  return (
-    <LikeHeartWrapper>
-      {like ? (<HeartIconSelect size="26" onClick={handleLike} />) : (<HeartIcon size="26" onClick={handleLike} />)}
-      <LikeText data-tip={nameTooltip} data-type="light" data-place="bottom" data-tip-disable={disabled}>{totalLikes}{totalLikes > 1 ? ' likes' : ' like'}</LikeText>
-      <ReactTooltip />
-    </LikeHeartWrapper>
-  )
+        let textTooltip = '';
+
+        const LOGGED_USER_LIKED = userLiked === true;
+        const TOTAL = likes.total - 2;
+        const CHECK_PLURAL = TOTAL > 1 ? '' : 's';
+
+        if (data.length > 1) {
+            LOGGED_USER_LIKED
+                ? (textTooltip += `Você, ${data[0].name}`)
+                : (textTooltip += `${data[0].name}, ${data[1].name}`);
+
+            TOTAL > 0
+                ? (textTooltip += ` e outra${CHECK_PLURAL} ${TOTAL} pessoa${CHECK_PLURAL}`)
+                : (textTooltip += ` curtiu`);
+        } else if (data.length === 1) {
+            LOGGED_USER_LIKED
+                ? (textTooltip += `Você e ${data[0].name}`)
+                : (textTooltip += `${data[0].name}`);
+
+            textTooltip += ` curtiu`;
+        } else {
+            LOGGED_USER_LIKED && (textTooltip += `Você curtiu`);
+        }
+
+        setNameTooltip(textTooltip);
+    }
+
+    return (
+        <LikeHeartWrapper>
+            {like ? (
+                <HeartIconSelect size="26" onClick={handleLike} />
+            ) : (
+                <HeartIcon size="26" onClick={handleLike} />
+            )}
+            <LikeText
+                data-tip={nameTooltip}
+                data-type="light"
+                data-place="bottom"
+                data-tip-disable={disabled}
+            >
+                {totalLikes}
+                {totalLikes > 1 ? ' likes' : ' like'}
+            </LikeText>
+            <ReactTooltip />
+        </LikeHeartWrapper>
+    );
 }
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,134 +1,145 @@
-import React, { useState, useRef, useEffect } from "react";
-import ReactHashtag from "@mdnm/react-hashtag";
+import React, { useState, useRef, useEffect } from 'react';
+import ReactHashtag from '@mdnm/react-hashtag';
 
 import {
-  PostBox,
-  PostContainer,
-  LeftContainer,
-  PerfilPicture,
-  RightContainer,
-  UserName,
-  TextDescription,
-  Hashtag,
-  TopContainer,
-  IconBox,
-  EditIcon,
-  DeleteIcon,
-  EditInput,
-} from "./style";
-import LinkPreview from "../LinkPreview";
-import LikeHeart from "../LikeHeart";
-import useAuth from "../../hooks/useAuth";
-import api from "../../services/api";
-import DeleteModal from "../DeleteModal";
+    PostBox,
+    PostContainer,
+    LeftContainer,
+    PerfilPicture,
+    RightContainer,
+    UserName,
+    TextDescription,
+    Hashtag,
+    TopContainer,
+    IconBox,
+    EditIcon,
+    DeleteIcon,
+    EditInput,
+} from './style';
+import LinkPreview from '../LinkPreview';
+import LikeHeart from '../LikeHeart';
+import useAuth from '../../hooks/useAuth';
+import api from '../../services/api';
+import DeleteModal from '../DeleteModal';
 
 export default function Post({
-  url,
-  linkTitle,
-  linkDescription,
-  linkImage,
-  textDescription,
-  postId,
-  author,
-  profilePicture,
-  userId,
+    url,
+    linkTitle,
+    linkDescription,
+    linkImage,
+    textDescription,
+    postId,
+    author,
+    profilePicture,
+    userId,
 }) {
-  const [like, setLike] = useState(false);
-  const { auth } = useAuth();
-  const [edit, setEdit] = useState(false);
-  const editRef = useRef();
-  const [editFocus, setEditFocus] = useState(false);
-  const [postText, setPostText] = useState(textDescription);
-  const [description, setDescription] = useState(postText);
-  const [loading, setLoading] = useState(false);
-  const [openModal, setOpenModal] = useState(false);
+    const [like, setLike] = useState(false);
+    const { auth } = useAuth();
+    const [edit, setEdit] = useState(false);
+    const editRef = useRef();
+    const [editFocus, setEditFocus] = useState(false);
+    const [postText, setPostText] = useState(textDescription);
+    const [description, setDescription] = useState(postText);
+    const [loading, setLoading] = useState(false);
+    const [openModal, setOpenModal] = useState(false);
 
-  useEffect(() => {
-    if (editFocus) {
-      editRef.current.focus();
-    }
-  }, [editFocus]);
+    useEffect(() => {
+        if (editFocus) {
+            editRef.current.focus();
+        }
+    }, [editFocus]);
 
-  function handleEditPost() {
-    if (edit) {
-      setDescription(postText);
-    }
-    setEditFocus(!editFocus);
-    setEdit(!edit);
-  }
-
-  async function keyEvents(e) {
-    if (e.keyCode === 27) {
-      handleEditPost();
-    }
-    if (e.keyCode === 13) {
-      setLoading(true);
-      const body = {
-        description,
-      };
-      try {
-        await api.editPost(postId, body, auth?.token);
-        setLoading(false);
-        setPostText(description);
-        setEdit(!edit);
+    function handleEditPost() {
+        if (edit) {
+            setDescription(postText);
+        }
         setEditFocus(!editFocus);
-      } catch (error) {
-        alert("Something went wrong, please try again");
-        setLoading(false);
-      }
+        setEdit(!edit);
     }
-  }
 
+    async function keyEvents(e) {
+        if (e.keyCode === 27) {
+            handleEditPost();
+        }
+        if (e.keyCode === 13) {
+            setLoading(true);
+            const body = {
+                description,
+            };
+            try {
+                await api.editPost(postId, body, auth?.token);
+                setLoading(false);
+                setPostText(description);
+                setEdit(!edit);
+                setEditFocus(!editFocus);
+            } catch (error) {
+                alert('Something went wrong, please try again');
+                setLoading(false);
+            }
+        }
+    }
 
-  return (
-    <PostBox>
-      {openModal && <DeleteModal openModal={openModal} setOpenModal={setOpenModal} postId={postId} />}
-      <PostContainer>
-        <LeftContainer>
-          <PerfilPicture src={profilePicture} />
-          <LikeHeart like={like} setLike={setLike} postId={postId} />
-        </LeftContainer>
-        <RightContainer>
-          <TopContainer>
-            <UserName to={`/user/${userId}`}>{author}</UserName>
-            {userId === auth.user.id && (
-              <IconBox>
-                <EditIcon size="20" onClick={handleEditPost} />
-                <DeleteIcon size="20" onClick={() => setOpenModal(true)} />
-              </IconBox>
+    return (
+        <PostBox key={postId}>
+            {openModal && (
+                <DeleteModal
+                    openModal={openModal}
+                    setOpenModal={setOpenModal}
+                    postId={postId}
+                />
             )}
-          </TopContainer>
-          {edit ? (
-            <EditInput
-              disabled={loading}
-              ref={editRef}
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              onKeyDown={keyEvents}
-            />
-          ) : (
-            <TextDescription>
-            <ReactHashtag
-              renderHashtag={(hashtagValue) => (
-                <Hashtag 
-                to={`/hashtag/${hashtagValue.replace("#","")}`}
-                >
-                  {hashtagValue}
-                </Hashtag>
-              )}
-            >
-            {postText}
-          </ReactHashtag>
-          </TextDescription>
-          )}
-          <LinkPreview
-            url={url}
-            linkTitle={linkTitle}
-            linkDescription={linkDescription}
-            linkImage={linkImage}
-          />
-        </RightContainer>
-      </PostContainer>
-    </PostBox>
-  );
+            <PostContainer>
+                <LeftContainer>
+                    <PerfilPicture src={profilePicture} />
+                    <LikeHeart like={like} setLike={setLike} postId={postId} />
+                </LeftContainer>
+                <RightContainer>
+                    <TopContainer>
+                        <UserName to={`/user/${userId}`}>{author}</UserName>
+                        {userId === auth?.user.id && (
+                            <IconBox>
+                                <EditIcon size="20" onClick={handleEditPost} />
+                                <DeleteIcon
+                                    size="20"
+                                    onClick={() => setOpenModal(true)}
+                                />
+                            </IconBox>
+                        )}
+                    </TopContainer>
+                    {edit ? (
+                        <EditInput
+                            disabled={loading}
+                            ref={editRef}
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                            onKeyDown={keyEvents}
+                        />
+                    ) : (
+                        <TextDescription>
+                            <ReactHashtag
+                                renderHashtag={(hashtagValue) => (
+                                    <Hashtag
+                                        to={`/hashtag/${hashtagValue.replace(
+                                            '#',
+                                            ''
+                                        )}`}
+                                    >
+                                        {hashtagValue}
+                                    </Hashtag>
+                                )}
+                            >
+                                {postText}
+                            </ReactHashtag>
+                        </TextDescription>
+                    )}
+                    <LinkPreview
+                        url={url}
+                        linkTitle={linkTitle}
+                        linkDescription={linkDescription}
+                        linkImage={linkImage}
+                    />
+                </RightContainer>
+            </PostContainer>
+        </PostBox>
+    );
 }
